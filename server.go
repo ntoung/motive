3f067cda23f3a990db49a218b38f0c2290890d27
ntoung <nmtoung@gmail.com>
2021-05-13 21:56:38 -0700

    Add csv reader

--- server.go
+++ server.go
@@ -1,10 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	err := http.ListenAndServe(":8080", nil)
+	csvfile, err := os.Open("./docs/Sample Data/motive_api_sample_data_customer_reviews.csv")
 	if err != nil {
-		panic(err)
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	r := csv.NewReader(csvfile)
+
+	for i := 0; i < 10; i++ {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Index: %s Text: %s", record[0], record[1])
 	}
 }
+
+// func NewReader(r io.Reader) *Reader {
+// 	return &Reader{
+// 		Comma: ',',
+// 		r:     bufio.NewReader(r),
+// 	}
+// }
